2ec20eff33b4ed4c65d32602a92b740c94ca0dae
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-09-18 11:33:50 +1000

    API Call to OpenWeather working

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,44 @@
+//Global const
+const cityInput = document.querySelector('#search-input'); //obtain input from HTML
+const previousInput = document.querySelector('#previous-list'); // obtain input from previous selection
+// const APIKey = aef8ff579a371781a816a273903f8295;
+
+
+const userCity = ""; // this is the users city selection
+const newCity = {}; // the city to be added history list
+const cityList = []; // list of cities previously searched
+
+
 //Search function
 // Attempt to get the user's search of city data if it exists
+
+function getUserCityChoice(previousCity){
+
+    var inputNewCity = {};
+
+    var count = 6;
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=brisbane&appid=aef8ff579a371781a816a273903f8295&units=metric&cnt=6";
+
+    console.log(queryURL);
+
+}
+
+getUserCityChoice();
+
+
+
 // store city name so that the search can be redone without typing in again
+function addCityToList(previousCity){
+    $inputNewCity= $("<li>");
+    $inputNewCity.attr("id",previousCity);
+    $inputNewCity.text(previousCity);
+
+    $previousInput.append($inputNewCity);
+    cityList.push(newCity);
+    console.log(cityList);
+}
+
 // present the historical search on side bar so that it can be selected
 
 
