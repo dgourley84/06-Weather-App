2497d281b89ba3388ba951992484345b9c4b8504
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-09-18 10:19:57 +1000

    pseudo code for index and html for function

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//Search function
+// Attempt to get the user's search of city data if it exists
+// store city name so that the search can be redone without typing in again
+// present the historical search on side bar so that it can be selected
+
+
+
+//if city is in search then obtain the weather
+// for each city obtain:
+// temp
+// humidity
+// windspeed
+// date
+// icon
+
+//present header box for current weather
+//create 5 days forecast in mini boxes
+
